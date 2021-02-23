98dbe8730d3790226752e39f770e59d225a78a3f
unknown <alexdang1993374@gmail.com>
2021-02-23 18:49:09 +0700

    setup redux

--- server/index.js
+++ server/index.js
@@ -7,12 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 const CONNECTION_URL =
   "mongodb+srv://alexdang1993374:Moblovesmilk1@cluster0.ywcy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
